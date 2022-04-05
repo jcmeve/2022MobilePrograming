158aa0d3c2e44bd43deff7be09ee9b49c789c2ad
user <gs09090@ajou.ac.kr>
2022-04-05 22:07:03 +0900

    남은수

--- app/src/main/java/com/example/mysprout/MainActivity.java
+++ app/src/main/java/com/example/mysprout/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.mysprout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //남은수
+    public void onClick1(View v) {
+        ConstraintLayout container = (ConstraintLayout) findViewById(R.id.container);
+        Intent intent = new Intent(getApplicationContext(), ch1.class);
+        startActivity(intent);
+
+    }
+
+    //남은수
+    public void onClick2(View v) {
+        ConstraintLayout container = (ConstraintLayout) findViewById(R.id.container);
+        Intent intent = new Intent(getApplicationContext(), ch2.class);
+        startActivity(intent);
+
     }
 }
