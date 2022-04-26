fdd2fb59f3651691f7a07c621c828699a2f90b47
jcm <jcmeve@naver.com>
2022-04-27 02:33:47 +0900

    Amplify pkg install

--- app/src/main/java/com/example/mysprout/MainActivity.java
+++ app/src/main/java/com/example/mysprout/MainActivity.java
@@ -5,19 +5,40 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
+import com.amplifyframework.core.Amplify;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        try {
+            Amplify.addPlugin(new AWSCognitoAuthPlugin());
+
+            Amplify.configure(getApplicationContext());
+            Log.i("MyAmplifyApp", "Initialized Amplify");
+        } catch (AmplifyException error) {
+            Log.e("MyAmplifyApp", "Could not initialize Amplify", error);
+        }
+        Amplify.Auth.fetchAuthSession(
+                result -> Log.i("AmplifyQuickstart", result.toString()),
+                error -> Log.e("AmplifyQuickstart", error.toString())
+        );
+
+
+
         setContentView(R.layout.activity_main);
     }
 
     //남은수
     public void onClick1(View v) {
-        ConstraintLayout container = (ConstraintLayout) findViewById(R.id.container);
         Intent intent = new Intent(getApplicationContext(), userinformation.class);
         startActivity(intent);
 
@@ -25,7 +46,6 @@ public class MainActivity extends AppCompatActivity {
 
     //남은수
     public void onClick2(View v) {
-        ConstraintLayout container = (ConstraintLayout) findViewById(R.id.container);
         Intent intent = new Intent(getApplicationContext(), sprout.class);
         startActivity(intent);
 
