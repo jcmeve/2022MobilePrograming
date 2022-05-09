f61936d3ffbdf7a81ff674e0ab5e74f3fc0810c7
jcm <jcmeve@naver.com>
2022-05-09 22:39:36 +0900

    sprout complete

--- app/src/main/java/com/example/mysprout/sprout.java
+++ app/src/main/java/com/example/mysprout/sprout.java
@@ -1,9 +1,9 @@
 package com.example.mysprout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.PopupMenu;
 import android.widget.TextView;
@@ -20,6 +20,7 @@ import java.util.concurrent.TimeUnit;
 public class sprout extends AppCompatActivity {
     static User getUserResult = null;
 
+    @SuppressLint("SetTextI18n")
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sprout);
@@ -27,14 +28,11 @@ public class sprout extends AppCompatActivity {
 
         DB.getInstance().GetUserInfo((result)->{
             getUserResult = result;
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    TextView days_txt = findViewById(R.id.todayText);
-                    Calendar curr = Calendar.getInstance();
-                    long days = TimeUnit.DAYS.convert(curr.getTime().getTime()/1000 - sprout.getUserResult.getMcreatedAt().getSecondsSinceEpoch(), TimeUnit.SECONDS);
-                    days_txt.setText("Day "+days);
-                }
+            runOnUiThread(() -> {
+                TextView days_txt = findViewById(R.id.todayText);
+                Calendar curr = Calendar.getInstance();
+                long days = TimeUnit.DAYS.convert(curr.getTime().getTime()/1000 - sprout.getUserResult.getMcreatedAt().getSecondsSinceEpoch(), TimeUnit.SECONDS);
+                days_txt.setText("Day "+days);
             });
         });
 
@@ -44,8 +42,10 @@ public class sprout extends AppCompatActivity {
 
     private void CalculateTodayCarbon(){
         _Action();
-
+        _Food();
+        _Transportation();
     }
+
     private void _Action(){
         DB.getInstance().GetUserActionHistory(
                 result->{
@@ -66,21 +66,57 @@ public class sprout extends AppCompatActivity {
         );
     }
 
+    private void _Food(){
+        DB.getInstance().GetUserFoodHistory(
+                result->{
+                    Calendar curr = Calendar.getInstance();
+
+                    int carbon = 0;
+                    for(int i = 0; i< result.length; i++){//종류
+                        for(int j = 0; j < result[i].food_history.getCount().size();j++){//각 기록
+                            long miles = result[i].food_history.getDate().get(j).getSecondsSinceEpoch()*1000;
+                            long days = TimeUnit.DAYS.convert(curr.getTime().getTime() - miles, TimeUnit.MILLISECONDS);
+                            if(days == 0) {
+                                carbon += result[i].food_history.getCount().get(j) * result[i].data.getCarbonPerUnit();
+                            }
+                        }
+                    }
+                    SetTodayCarbonTxt(carbon);
+                }
+        );
+    }
+
+    private void _Transportation(){
+        DB.getInstance().GetUserTransportationHistory(
+                result->{
+                    Calendar curr = Calendar.getInstance();
+
+                    int carbon = 0;
+                    for(int i = 0; i< result.length; i++){//종류
+                        for(int j = 0; j < result[i].transportation_history.getCount().size();j++){//각 기록
+                            long miles = result[i].transportation_history.getDate().get(j).getSecondsSinceEpoch()*1000;
+                            long days = TimeUnit.DAYS.convert(curr.getTime().getTime() - miles, TimeUnit.MILLISECONDS);
+                            if(days == 0) {
+                                carbon += result[i].transportation_history.getCount().get(j) * result[i].data.getCarbonPerUnit();
+                            }
+                        }
+                    }
+                    SetTodayCarbonTxt(carbon);
+                }
+        );
+    }
+
     static int total = 0;
+    @SuppressLint("SetTextI18n")
     public synchronized void SetTodayCarbonTxt(int value){
         total += value;
         Log.i("tatasta",total+"");
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                TextView txt = findViewById(R.id.estimateText);
-                txt.setText(sprout.total+"");
-            }
+        runOnUiThread(() -> {
+            TextView txt = findViewById(R.id.estimateText);
+            txt.setText(sprout.total+"");
         });
 
     }
-
-
 
 
     //기록 버튼 눌렀을 때 습관 기록과 식단 기록 중 어디로 넘어갈지 팝업 메뉴로 선택
@@ -88,20 +124,18 @@ public class sprout extends AppCompatActivity {
         PopupMenu chooseRecord = new PopupMenu(this, button);
         chooseRecord.getMenuInflater().inflate(R.menu.record_popup, chooseRecord.getMenu());
         chooseRecord.setOnMenuItemClickListener(
-                new PopupMenu.OnMenuItemClickListener(){
-                    public boolean onMenuItemClick(MenuItem item){
-                        if(item.getItemId() == R.id.record_habit){
-                            Toast.makeText(sprout.this, "습관 기록으로 넘어갑니다.", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(sprout.this, RecordHabits.class);
-                            startActivity(intent);
-                        }
-                        else if(item.getItemId() == R.id.record_food){
-                            Toast.makeText(sprout.this, "식단 기록으로 넘어갑니다.", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(sprout.this, RecordFood.class);
-                            startActivity(intent);
-                        }
-                        return true;
+                item -> {
+                    if(item.getItemId() == R.id.record_habit){
+                        Toast.makeText(sprout.this, "습관 기록으로 넘어갑니다.", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(sprout.this, RecordHabits.class);
+                        startActivity(intent);
                     }
+                    else if(item.getItemId() == R.id.record_food){
+                        Toast.makeText(sprout.this, "식단 기록으로 넘어갑니다.", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(sprout.this, RecordFood.class);
+                        startActivity(intent);
+                    }
+                    return true;
                 });
         chooseRecord.show();
     }
@@ -111,22 +145,20 @@ public class sprout extends AppCompatActivity {
         PopupMenu popup = new PopupMenu(this, button4);
         popup.getMenuInflater().inflate(R.menu.popup, popup.getMenu());
         popup.setOnMenuItemClickListener(
-                new PopupMenu.OnMenuItemClickListener() {
-                    public boolean onMenuItemClick(MenuItem item) {
-                        if (item.getItemId() == R.id.action_menu1) {
-                            Toast.makeText(sprout.this, "메뉴 1 클릭", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(getApplicationContext(), nickname.class);
-                            startActivity(intent);
-                        } else if (item.getItemId() == R.id.action_menu2) {
-                            Toast.makeText(sprout.this, "메뉴 2 클릭", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(getApplicationContext(), mysprout.class);
-                            startActivity(intent);
-                        } else {
-                            Toast.makeText(sprout.this, "메뉴 3 클릭", Toast.LENGTH_SHORT).show();
+                item -> {
+                    if (item.getItemId() == R.id.action_menu1) {
+                        Toast.makeText(sprout.this, "메뉴 1 클릭", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), nickname.class);
+                        startActivity(intent);
+                    } else if (item.getItemId() == R.id.action_menu2) {
+                        Toast.makeText(sprout.this, "메뉴 2 클릭", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), mysprout.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(sprout.this, "메뉴 3 클릭", Toast.LENGTH_SHORT).show();
 
-                        }
-                        return false;
                     }
+                    return false;
                 });
         popup.show();
     }
