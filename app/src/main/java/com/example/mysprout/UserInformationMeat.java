54ce71e639d1264b54a576c9646523fed31172ca
jcm <jcmeve@naver.com>
2022-05-12 00:16:33 +0900

    set meat carbon

--- app/src/main/java/com/example/mysprout/UserInformationMeat.java
+++ app/src/main/java/com/example/mysprout/UserInformationMeat.java
@@ -2,27 +2,63 @@ package com.example.mysprout;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.RadioGroup;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.amplifyframework.datastore.generated.model.MeatLevelData;
+
 public class UserInformationMeat extends AppCompatActivity {
+    private enum MEAT_LEVEL{
+        HIGH,LOW,NO,NULL
+    }
+
+    static MEAT_LEVEL meatLevel = MEAT_LEVEL.NULL;
+    static MeatLevelData meatLevelData;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.userinformation_meat);
 
-        Button nextBtn;
-        nextBtn = (Button)findViewById(R.id.nextbtn_meat);
-        nextBtn.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent intent = new Intent(UserInformationMeat.this, UserInformationTransport.class);
-                startActivity(intent);
+        DB.getInstance().msGetMeatLevelData(data -> UserInformationMeat.meatLevelData = data);
+
+        RadioGroup radioGroup = findViewById(R.id.meat_radio_group);
+        radioGroup.setOnCheckedChangeListener((radioGroup1, id) -> {
+            if (id == R.id.highMeat_btn) {
+                UserInformationMeat.meatLevel = MEAT_LEVEL.HIGH;
+            } else if (id == R.id.lowMeat_btn) {
+                UserInformationMeat.meatLevel = MEAT_LEVEL.LOW;
+            } else if (id == R.id.noMeat_btn) {
+                UserInformationMeat.meatLevel = MEAT_LEVEL.NO;
+            } else {
+                Log.e("ERROR", "SELECT MEAT");
             }
         });
-        //TODO: MeatLevelData table을 받아 SetMeatCarbon로 탄소량 저장
+
+        Button nextBtn;
+        nextBtn = (Button)findViewById(R.id.nextbtn_meat);
+        nextBtn.setOnClickListener(v -> {
+            switch (meatLevel){
+                case HIGH:
+                    DB.getInstance().SetMeatCarbon(meatLevelData.getHighCarbon());
+                    break;
+                case LOW:
+                    DB.getInstance().SetMeatCarbon(meatLevelData.getLowCarbon());
+                    break;
+                case NO:
+                    DB.getInstance().SetMeatCarbon(meatLevelData.getNoCarbon());
+                    break;
+                default:
+                    Log.e("ERROR", "SELECT MEAT");
+            }
+
+
+            Intent intent = new Intent(UserInformationMeat.this, UserInformationTransport.class);
+            startActivity(intent);
+        });
     }
 
 
