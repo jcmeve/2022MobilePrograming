bf9dba832e06995fd01d62a76e3fb1e694203605
jcm <jcmeve@naver.com>
2022-05-12 01:03:34 +0900

    set meat carbon

--- app/src/main/java/com/example/mysprout/UserInformationMeat.java
+++ app/src/main/java/com/example/mysprout/UserInformationMeat.java
@@ -3,6 +3,7 @@ package com.example.mysprout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RadioGroup;
 
