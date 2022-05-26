b2ba30ca2cab777f05b900853090c6df778e911a
jcm <jcmeve@naver.com>
2022-05-26 23:04:57 +0900

    step counter complete

--- app/src/main/java/com/example/mysprout/StepTest.java
+++ app/src/main/java/com/example/mysprout/StepTest.java
@@ -82,7 +82,7 @@ public class StepTest extends AppCompatActivity {
         long startTime = cal.getTimeInMillis();
 
         // 종료 시간
-        cal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH),
+        cal.set(cal.get(Calendar.YEAR)+1, cal.get(Calendar.MONTH),
                 cal.get(Calendar.DAY_OF_MONTH), 23, 0, 0);
         long endTime = cal.getTimeInMillis();
         String TAG = "GOOGLE FIT";
