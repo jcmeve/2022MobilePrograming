102ce81837ea1bd5f5bddf3ed87b84b393cb3f22
JH-kor <64531253+JH-kor@users.noreply.github.com>
2022-05-30 20:23:04 +0900

    Record Step Activity ~ dialog ~ Notification

--- app/src/main/java/com/example/mysprout/RecordStep.java
+++ app/src/main/java/com/example/mysprout/RecordStep.java
@@ -1,15 +1,34 @@
 package com.example.mysprout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.view.View;
+import android.widget.ToggleButton;
 
 import com.example.mysprout.databinding.RecordStepBinding;
 import com.example.mysprout.fragment.DialogChooseTransportation;
 
-public class RecordStep extends AppCompatActivity {
+import java.io.InputStream;
 
+public class RecordStep extends AppCompatActivity implements DialogChooseTransportation.ChosenDataListener {
+    //뷰 바인딩
     RecordStepBinding stepBinding;
+    String chosenTransport;
+    NotificationManager notificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +36,102 @@ public class RecordStep extends AppCompatActivity {
         stepBinding = RecordStepBinding.inflate(getLayoutInflater());
         setContentView(stepBinding.getRoot());
 
+        chosenTransport = "";
+        showDialog();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        stepBinding.recordStepButtonStartAndStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean checked = ((ToggleButton)view).isChecked();
+
+                if(checked){
+                    stepBinding.recordStepButtonStartAndStop.setTextColor(getResources().getColor(R.color.white));
+                    //서비스(걸음 수 측정) 시작
+                    callNotification();
+                }else{
+                    //종료 묻는 다이얼로그 띄운 후 서비스(걸음 수 측정) 종료
+                }
+            }
+        });
+    }
+
+    void showDialog(){
         DialogChooseTransportation dialog = new DialogChooseTransportation();
 
+        dialog.setChosenDataListener(new DialogChooseTransportation.ChosenDataListener() {
+            @Override
+            public void giveData(String tag) {
+                if(tag.equals("CANCEL")){
+                    finish();
+                }else{
+                    stepBinding.recordStepTextTransport.setText(tag + " 대신 걷기!");
+                    stepBinding.recordStepTextTransport.setText(emphasizeText(tag));
+
+                    chosenTransport = tag;
+                }
+            }
+        });
+
         dialog.show(getSupportFragmentManager(), "choose transportation dialog");
+    }
+
+    SpannableString emphasizeText(String word){
+
+        String content = stepBinding.recordStepTextTransport.getText().toString();
+
+        SpannableString spannableString = new SpannableString(content);
+        int start = content.indexOf(word);
+        int end = start + word.length();
+
+        spannableString.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.mintGreen)),
+                start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new RelativeSizeSpan(1.3f), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        return spannableString;
+    }
+
+    @Override
+    public void giveData(String tag) {
+
+    }
+
+    //여기서부터 Notification 생성 메소드
+    public void callNotification(){
+        NotificationCompat.Builder builder = getBuilder("channel_1", "noti_record_steps");
+        builder.setSmallIcon(android.R.drawable.ic_notification_overlay); //임시 small 아이콘
+        builder.setContentTitle("0,000걸음"); //걸음 수 업데이트
+        builder.setContentText(chosenTransport + " 대신 걸어서 환경 보호"); //사용자 선택한 이동수단 따라 텍스트 변경
+        Bitmap bitmapWalking = BitmapFactory.decodeResource(getResources(), R.drawable.icon_baseline_walk);
+        builder.setLargeIcon(bitmapWalking);
+        builder.setDefaults(Notification.DEFAULT_VIBRATE);
+
+        //PendingIntent로 noti누르면 RecordStep 액티비티로 돌아오도록 시스템에 의뢰
+        Intent intent = new Intent(this, RecordStep.class);
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(this, 1000, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(pendingIntent);
+
+        Notification notification = builder.build();
+
+        notificationManager.notify(2000, notification);
+    }
+
+    public NotificationCompat.Builder getBuilder(String channel_id, String channel_name){
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = null;
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel =
+                    new NotificationChannel(channel_id, channel_name, NotificationManager.IMPORTANCE_HIGH);
+            channel.enableVibration(true); //일단 진동만
+            notificationManager.createNotificationChannel(channel);
+            builder = new NotificationCompat.Builder(this, channel_id);
+        }else{
+            builder = new NotificationCompat.Builder(this, channel_id);
+        }
+        return builder;
     }
 }
--- app/src/main/java/com/example/mysprout/fragment/DialogChooseTransportation.java
+++ app/src/main/java/com/example/mysprout/fragment/DialogChooseTransportation.java
@@ -1,28 +1,125 @@
 package com.example.mysprout.fragment;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
+import android.os.Debug;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import com.example.mysprout.R;
+import com.example.mysprout.RecordStep;
+
+import java.util.Objects;
 
 
 public class DialogChooseTransportation extends DialogFragment {
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    String chosenTransport;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dialog_choose_transportation, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Objects.requireNonNull(getDialog()).getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        chosenTransport = "";
+
+        Button okBtn = view.findViewById(R.id.dialog_choose_transport_ok);
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!chosenTransport.isEmpty() && chosenDataListener != null){
+                    chosenDataListener.giveData(chosenTransport);
+                    Toast.makeText(getContext(), chosenTransport + "선택", Toast.LENGTH_SHORT).show();
+                    dismiss();
+                }else if(chosenTransport.isEmpty()){
+                    Toast.makeText(getContext(), "이동 수단을 선택하세요.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        Button cancelBtn = view.findViewById(R.id.dialog_choose_transport_cancel);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chosenTransport = "";
+                if(chosenDataListener != null){
+                    chosenDataListener.giveData("CANCEL");
+                }
+                dismiss();
+            }
+        });
+
+        RadioButton carBtn = view.findViewById(R.id.dialog_choose_transport_car);
+        carBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean checked = ((RadioButton) view).isChecked();
+                if(checked){
+                    chosenTransport = "승용차";
+                }
+            }
+        });
+
+//        RadioButton carBtn = view.findViewById(R.id.dialog_choose_transport_car);
+//        carBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+//                if(b){
+//                    chosenTransport = "CAR";
+//                    Log.d("CHOOSE", chosenTransport);
+//                }
+//            }
+//        });
+
+        RadioButton busBtn = view.findViewById(R.id.dialog_choose_transport_bus);
+        busBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean checked = ((RadioButton) view).isChecked();
+                if(checked){
+                    chosenTransport = "버스";
+                }
+            }
+        });
+
+        RadioButton subwayBtn = view.findViewById(R.id.dialog_choose_transport_subway);
+        subwayBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean checked = ((RadioButton) view).isChecked();
+                if(checked){
+                    chosenTransport = "지하철";
+                }
+            }
+        });
+    }
+
+    private  ChosenDataListener chosenDataListener;
+    public void setChosenDataListener(ChosenDataListener listener){
+        chosenDataListener = listener;
+    }
+
+    //액티비티에 정보 전달할 인터페이스
+    public interface ChosenDataListener{
+        void giveData(String tag);
     }
 }
