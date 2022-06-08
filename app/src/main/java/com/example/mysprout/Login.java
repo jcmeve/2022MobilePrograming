957b7e1116f17932e96809112e4446107f150031
jcm <jcmeve@naver.com>
2022-06-09 04:40:49 +0900

    GoogleFit Init Fix

--- app/src/main/java/com/example/mysprout/Login.java
+++ app/src/main/java/com/example/mysprout/Login.java
@@ -30,9 +30,7 @@ import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.fitness.FitnessOptions;
 import com.google.android.gms.fitness.data.DataType;
 public class Login extends AppCompatActivity {
-    final int GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 1;
-    FitnessOptions fitnessOptions;
-    GoogleSignInAccount account;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,7 +49,6 @@ public class Login extends AppCompatActivity {
         });
 
 
-        GoogleFitInit();
 
         nextBtn.setOnClickListener(v -> tryLogin());
 
@@ -81,19 +78,6 @@ public class Login extends AppCompatActivity {
     }
 
 
-    ActivityResultLauncher<Intent> launcher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult result) {
-                    Log.i("RESULT CODE : ", ""+result.toString());
-                    if(result.getResultCode() == 123){
-                        Log.i("GOOGLE","LOGIN SUCCESS");
-                    }
-
-
-                }
-            });
 
 
     private void loginProgress(boolean result){
@@ -107,83 +91,6 @@ public class Login extends AppCompatActivity {
 
         }
 
-    }
-
-    private void GoogleFitInit() {
-
-
-        account =  GoogleSignIn.getLastSignedInAccount(getApplicationContext());
-        Log.i("GOOGLE","acount: " + account);
-        if(account == null){
-
-            GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                    .requestEmail()
-                    .build();
-            GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-            launcher.launch(signInIntent);
-
-        }
-        fitnessOptions = FitnessOptions.builder()
-                .addDataType(DataType.TYPE_STEP_COUNT_DELTA)
-                .addDataType(DataType.TYPE_DISTANCE_DELTA)
-                .addDataType(DataType.TYPE_CALORIES_EXPENDED)
-                .build();
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, GOOGLE_FIT_PERMISSIONS_REQUEST_CODE);
-        }
-        if (!GoogleSignIn.hasPermissions(account, fitnessOptions)) {
-            GoogleSignIn.requestPermissions(
-                    this,
-                    GOOGLE_FIT_PERMISSIONS_REQUEST_CODE,
-                    account,
-                    fitnessOptions
-            );
-            Log.i("GIGIGIGI", "sadfasfasf");
-        } else {
-            accessGoogleFit();
-        }
-
-    }
-
-    private void accessGoogleFit() {
-
-        Fitness.getRecordingClient(this, GoogleSignIn.getAccountForExtension(getApplicationContext(), fitnessOptions))
-                .subscribe(DataType.TYPE_STEP_COUNT_DELTA)
-                .addOnCompleteListener(
-                        var -> Log.i("SUCCESS", "Successfully subscribed!")
-                ).addOnFailureListener(
-                e -> Log.w("ERROR", "There was a problem subscribing.", e)
-        );
-        Fitness.getRecordingClient(this, GoogleSignIn.getAccountForExtension(getApplicationContext(), fitnessOptions))
-                .subscribe(DataType.TYPE_DISTANCE_DELTA)
-                .addOnCompleteListener(
-                        var -> Log.i("SUCCESS", "Successfully subscribed!")
-                ).addOnFailureListener(
-                e -> Log.w("ERROR", "There was a problem subscribing.", e)
-        );
-        Fitness.getRecordingClient(this, GoogleSignIn.getAccountForExtension(getApplicationContext(), fitnessOptions))
-                .subscribe(DataType.TYPE_CALORIES_EXPENDED)
-                .addOnCompleteListener(
-                        var -> Log.i("SUCCESS", "Successfully subscribed!")
-                ).addOnFailureListener(
-                e -> Log.w("ERROR", "There was a problem subscribing.", e)
-        );
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(resultCode == Activity.RESULT_OK){
-            if(requestCode == GOOGLE_FIT_PERMISSIONS_REQUEST_CODE) {
-                accessGoogleFit();
-
-                //    readData();
-            }
-
-        }
     }
 
 }
